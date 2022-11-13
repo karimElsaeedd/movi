7a81f380198e00f82b3f732166d006e039503726
Karim Elsaeed <karimelsaeedd@gmail.com>
2022-11-13 19:36:12 +0200

    navbar with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NetworkComponent } from './network/network.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PeopleComponent } from './people/people.component';
+import { RegisterComponent } from './register/register.component';
+import { TvshowsComponent } from './tvshows/tvshows.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'movies',component:MoviesComponent},
+  {path:'tvshows',component:TvshowsComponent},
+  {path:'details',component:DetailsComponent},
+  {path:'people',component:PeopleComponent},
+  {path:'network',component:NetworkComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'**',component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
