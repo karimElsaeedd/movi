e9267894d75114a21825343af40c9b8f8601a173
Karim Elsaeed <karimelsaeedd@gmail.com>
2022-11-13 17:57:42 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvshowsComponent } from './tvshows/tvshows.component';
+import { PeopleComponent } from './people/people.component';
+import { NetworkComponent } from './network/network.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailsComponent } from './details/details.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    MoviesComponent,
+    TvshowsComponent,
+    PeopleComponent,
+    NetworkComponent,
+    NotfoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
